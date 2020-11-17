5613bdc16e482baac566d6ae2cae02982057c02e
Aldrin Gonzalez Cancino <sounix@gmail.com>
2020-11-17 14:07:13 -0600

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.json());
 
 const mockUserData = [
   { name: "Aldrin" },
@@ -22,6 +26,26 @@ app.get("/users/:id", (req, res) => {
     message: "got one user",
     user: req.params.id,
   });
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const mockUsername = "billyTheKid";
+  const mockPassword = "superSecret";
+
+  if (username === mockUsername && password === mockPassword) {
+    res.json({
+      success: true,
+      message: "password and username match!",
+      token: "encrypted token goes here",
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "password and username do not match",
+    });
+  }
 });
 
 app.listen(8000, () => {
