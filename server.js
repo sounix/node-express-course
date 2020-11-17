42022d453e150039a4f5a907e57f6bf9e8359207
Aldrin Gonzalez Cancino <sounix@gmail.com>
2020-11-17 13:23:03 -0600

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 
+const mockUserData = [
+  { name: "Aldrin" },
+  { name: "Cindy" },
+  { name: "Edward" },
+];
+
+app.get("/users", (req, res) => {
+  res.json({
+    success: true,
+    message: "successfully got user. Nice!",
+    users: mockUserData,
+  });
+});
+
 app.listen(8000, () => {
   console.log("server is running");
 });
